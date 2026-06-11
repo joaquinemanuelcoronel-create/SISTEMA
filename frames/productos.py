2d44bd36388128e3724d8af610d8269c744bac14
Your Name <jeremiaserhardt6@gmail.com>
2026-06-11 14:17:27 -0300

    Agregue etiqueta

--- frames/productos.py
+++ frames/productos.py
@@ -12,3 +12,16 @@ def crear_productos(padre):
     ).pack(pady=30)
 
     return frame
+
+def crear_productos(padre):
+    frame = tk.Frame(padre, bg="#f2f2f2")
+
+    tk.Label(
+        frame,
+        text="Lavandina",
+        font=("Arial", 24, "bold"),
+        bg="#f2f2f2",
+        fg="#2c3e50"
+    ).pack(pady=30)
+
+    return frame
